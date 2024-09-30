5ff41e2b7d04b5241efc80d37fde9f2d1a783515
David Binns <dbinns.web+git@gmail.com>
2024-09-30 14:49:54 +0100

    Preserve environment

--- sudo.js
+++ sudo.js
@@ -5,7 +5,7 @@ import { userInfo } from "node:os";
 export function sudo() {
     if (userInfo().uid != 0) {
         console.error("Re-running with sudo", process.argv);
-        let { status } = spawnSync("sudo", process.argv, { stdio: 'inherit' });
+        let { status } = spawnSync("sudo", ["-E", ...process.argv], { stdio: 'inherit' });
         process.exit(status);
     }
 }
